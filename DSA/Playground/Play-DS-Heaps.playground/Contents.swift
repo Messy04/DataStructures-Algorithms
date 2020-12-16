dfdd3b559ce914031d2ba6865813dcda26b4567f
Messy04 <singhrishab53545@gmail.com>
2020-12-16 14:59:49 +0530

    Modified folder names and Added SOLID Principle folder

--- DSA/Playground/Play-DS-Heaps.playground/Contents.swift
+++ DSA/Playground/Play-DS-Heaps.playground/Contents.swift
@@ -1,1 +1,27 @@
 /* Implementation of Heap Data Structure */
+
+// MARK:- /* Heap class with sort and heapify */
+class Heap {
+    
+    private var array: [Int]
+    private var size: Int
+    
+    init(array: [Int]) {
+        self.array = array
+        self.size = array.count
+    }
+    
+    func sort() {
+        
+    }
+     
+    private func heapify() {
+          
+    }
+}
+
+// MARK:- /* DRIVER PROGRAM */
+let array = [5,2,1,7,4,6,3]
+
+let heap = Heap(array: array)
+heap.sort()
