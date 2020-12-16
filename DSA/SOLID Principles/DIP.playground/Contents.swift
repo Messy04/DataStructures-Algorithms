8c45f1698bacf7b12cac9177e17627a98824ae2f
Messy04 <singhrishab53545@gmail.com>
2020-12-16 23:31:26 +0530

    Add DIP file implementation

--- DSA/SOLID Principles/DIP.playground/Contents.swift
+++ DSA/SOLID Principles/DIP.playground/Contents.swift
@@ -2,3 +2,36 @@
 
 import Foundation
 
+// MARK:- low level module
+class FileSystemManager {
+    func save(string: String) {
+        //
+    }
+}
+
+// MARK: High level module depends on Low level module its tightly coupled
+class Handler {
+    let fm = FileSystemManager()
+    
+    func handle(string: String) {
+        fm.save(string: string)
+    }
+}
+
+// MARK:- DIP Way
+protocol Storage {
+    func save(str: String)
+}
+
+class FSM: Storage {
+    func save(str: String) {
+        //
+    }
+}
+
+class DBM: Storage {
+    func save(str: String) {
+        //
+    }
+}
+
